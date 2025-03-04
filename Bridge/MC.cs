89d6a3eb3608737733d12d88bcee5b29180f8b83
Jonas Büchner <JonasBuchner36@gmail.com>
2025-03-04 13:35:24 +0100

    Task 3

---
+++ Bridge/MC.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    /// <summary>
+    /// Repræsenterer en motorcykel med en nummerplade og en dato.
+    /// </summary>
+    public class MC
+    {
+        /// <summary>
+        /// Får eller sætter motorcyklens nummerplade.
+        /// </summary>
+        public string Licenseplate;
+
+        /// <summary>
+        /// Får eller sætter motorcyklens dato.
+        /// </summary>
+        public DateTime date;
+
+        /// <summary>
+        /// Returnerer prisen på motorcyklen.
+        /// </summary>
+        /// <returns>Prisen på motorcyklen, som er fastsat til 120.</returns>
+        public double Price()
+        {
+            return 120;
+        }
+
+        /// <summary>
+        /// Returnerer køretøjstypen.
+        /// </summary>
+        /// <returns>En streng, der repræsenterer køretøjstypen, som er "MC".</returns>
+        public string vihicleType()
+        {
+            return "MC";
+        }
+    }
+}
---
+++ BridgeTests/MCTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bridge;
+using System;
+
+namespace MCUnitTestAfAlleMetoder
+{
+    [TestClass()]
+    public class MCUnitTestAfAlleMetoder
+    {
+        [TestMethod()]
+        public void PriceTest()
+        {
+            // Arrange
+            var mC = new MC();
+
+            // Act
+            var price = mC.Price();
+
+            // Assert
+            Assert.AreEqual(120, price);
+        }
+
+        [TestMethod()]
+        public void VehicleTypeTest()
+        {
+            // Arrange
+            var mC = new MC();
+
+            // Act
+            var vehicleType = mC.vihicleType();
+
+            // Assert
+            Assert.AreEqual("MC", vehicleType);
+        }
+    }
+}
